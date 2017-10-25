0c5b855d1601a1d318a234063bcbb5b0b10d9c6c
gentom <gento.m.developer@gmail.com>
2017-10-25 23:03:32 +0900

    commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+
+	InputField inputField;
+
+
+	/// <summary>
+	/// Startメソッド
+	/// InputFieldコンポーネントの取得および初期化メソッドの実行
+	/// </summary>
+	void Start ()
+	{
+
+		inputField = GetComponent<InputField> ();
+
+		InitInputField ();
+	}
+
+
+
+	/// <summary>
+	/// Log出力用メソッド
+	/// 入力値を取得してLogに出力し、初期化
+	/// </summary>
+
+
+	public void InputLogger ()
+	{
+
+		string inputValue = inputField.text;
+
+		Debug.Log (inputValue);
+
+		InitInputField ();
+	}
+
+
+
+	/// <summary>
+	/// InputFieldの初期化用メソッド
+	/// 入力値をリセットして、フィールドにフォーカスする
+	/// </summary>
+
+
+	void InitInputField ()
+	{
+
+		// 値をリセット
+		inputField.text = "";
+
+		// フォーカス
+		inputField.ActivateInputField ();
+	}
+
+}
