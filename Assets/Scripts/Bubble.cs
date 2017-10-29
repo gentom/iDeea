641e07c95c89497d1894705170d63898889d2917
chankou0920 <chankou.app@gmail.com>
2017-10-29 18:30:41 +0900

    add BubbleCreator

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // 追加
+
+public class Bubble : MonoBehaviour {
+
+	[SerializeField] public TextMesh textMesh;
+	Vector3 screenPoint;
+	Vector3 offset;
+	
+	public void Set (string text)
+	{
+		textMesh.text = text;
+	}
+
+	// タッチされた時
+	void OnMouseDown ()
+	{
+		print ("down");
+		// マウスカーソルは、スクリーン座標なので、
+		// 対象のオブジェクトもスクリーン座標に変換してから計算する。
+
+		// このオブジェクトの位置(transform.position)をスクリーン座標に変換。
+		screenPoint = Camera.main.WorldToScreenPoint (transform.position);
+		// ワールド座標上の、マウスカーソルと、対象の位置の差分。
+		offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+
+	// ドラッグ中
+	void OnMouseDrag ()
+	{
+		Vector3 currentScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 currentPosition = Camera.main.ScreenToWorldPoint (currentScreenPoint) + this.offset;
+		transform.position = currentPosition;
+	}
+
+	// タッチが離れた時
+	void OnMouseUp ()
+	{
+		print ("up");
+	}
+
+	// frameと重なった時の処理
+	void OnTriggerEnter2D (Collider2D bubble) {
+		print ("重なった");
+		print(bubble.gameObject.GetComponent<Bubble> ().textMesh.text);
+	}
+}
---
+++ Assets/Scripts/BubbleCreator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;  // 追加
+
+public class BubbleCreator : MonoBehaviour {
+
+	const float radius = 2.0f;  // 泡が広がる時の円の半径
+	const float time = 1.5f;  // 泡が広がる時間
+
+	[SerializeField] GameObject prefab;  // 泡のprefab
+	[SerializeField] GameObject bubbleParent;  // 泡の親
+
+	string[] s = {
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+		"g",
+		"h",
+		"i",
+		"j",
+	};
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	// InputFieldに入力されたテキストを受け取る
+	public void InputText (string text)
+	{
+		Debug.Log (text);
+		CreateBubbles (Vector3.zero);
+	}
+
+	void CreateBubbles (Vector3 pos)
+	{
+		float radian = (360f / (float)s.Length) * Mathf.PI / 180f;
+		for (int i = 0; i < s.Length; i++) {
+			GameObject obj = (GameObject)Instantiate (
+				prefab,
+				pos,
+				Quaternion.identity
+			);
+			obj.transform.parent = bubbleParent.transform;
+
+			// 泡にテキストをセットする
+			Bubble bubble = obj.GetComponent<Bubble> ();
+			bubble.Set (s[i]);
+
+			// 泡が広がる動き
+//			Sequence sequence = DOTween.Sequence();
+			obj.transform.DOMove (
+				new Vector3 (pos.x + Mathf.Cos(radian * i) * radius, pos.y + Mathf.Sin(radian * i) * radius, 0),
+				time
+			).SetEase(Ease.OutCirc);
+		}
+	}
+}
