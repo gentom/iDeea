04bf945afda37a0abd06527eefc870e1e091a058
gentom <gento.m.developer@gmail.com>
2017-10-29 15:18:40 +0900

    commit

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class Client : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+}
